b6936091f9739b5c02fc3d9baa635ee3b2de2995
aiqiliu <aiqiliu2018@u.northwestern.edu>
2016-06-21 11:19:10 -0500

    transform into correct format

--- convertInline.py
+++ convertInline.py
@@ -1,28 +1,36 @@
 # convert csv to influx write type
 # format: <tableTname> filed1= filed2= ... timestamp
-import os, csv
-lst=os.listdir("./btds") # get filename -> tablename in db
+import os, csv, sys
+lst=os.listdir("../btds") # get filename -> tablename in db
 
 
 for f in lst:
-	# create a txt file for each csv file
-	txt = []
-	with open('./btds/' + f, 'rU') as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			#parse throguh each row
-			line = []
-			line.append(f[:-4]) #filename -> tableName
-			for key in row.keys()[1:]: #skip timestamp and append at last
-				# parse through each column
-				line.append(str(key) + '=' + str(row[key]))
-			line = ','.join(line)
-			line += ' ' + str(row[row.keys()[0]])
-			txt.append(line)
-
-	txt = '\n'.join(txt)
-
-	print txt	
+	if f != '.DS_Store':
+		print "Reading into " + f
+		# create a txt file for each csv file
+		txt = []
+		with open('../btds/' + f, 'rb') as csvfile:
+			reader = csv.DictReader((line.replace('\0','') for line in csvfile))
+			headers = reader.fieldnames
+			for row in reader:
+				#parse throguh each row
+				line = []
+				line.append(f[:-4]) #filename -> tableName
+				for header in headers[1:]: #skip timestamp and append at last
+					# parse through each column
+					line.append(str(header) + '=' + str(row[header]))
+					# print str(header) + '=' + str(row[header])
+				line = ','.join(line)
+				line += ' ' + str(row[headers[0]])
+				txt.append(line)
+		txt = '\n'.join(txt)
+		if not os.path.exists('./output'): 
+			os.makedirs('./output')
+		file = open('./output/' + f[:-4] + '.txt', "w")
+		file.write(txt)
+		print "file saved at " + './output/' + f[:-4] + '.txt'
+		file.close	
+			
 	
 
 
